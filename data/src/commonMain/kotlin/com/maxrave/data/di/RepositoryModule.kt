d9820d4f3e8e88577cdb2bbcba03ae25dead8815
thecyberix <299089191+thecyberix@users.noreply.github.com>
2026-08-03 14:37:49 +0300

    feat(android-auto): like in previous slot with custom prev overflow
    Restore point for AA Like-instead-of-Prev: Like in SLOT_BACK, stock-looking Previous in overflow, wheel previous remapped to like, gated by setting (default on).

--- data/src/commonMain/kotlin/com/maxrave/data/di/RepositoryModule.kt
+++ data/src/commonMain/kotlin/com/maxrave/data/di/RepositoryModule.kt
@@ -78,7 +78,7 @@ val repositoryModule =
         }
 
         single<SongRepository>(createdAtStart = true) {
-            SongRepositoryImpl(get(), get(), get())
+            SongRepositoryImpl(get(), get(), get(), get())
         }
 
         single<StreamRepository>(createdAtStart = true) {
